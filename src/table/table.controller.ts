29591c8639e1727c8050ffe56a602f150abaeb8a
mthiagoalves <m.thiago.ales@live.com>
2022-05-19 20:06:55 -0300

    feat(table):implement findOne

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CreateTableDto } from "./dto/create-table.dto";
+import { Table } from "./entities/table.entity";
 import { TableService } from "./table.service";
 
 @ApiTags('Table')
@@ -9,13 +10,26 @@ export class TableController {
   constructor( private readonly tableService: TableService) {}
 
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary: 'List all tables'
+  })
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
+  }
 
+  @Get(':id')
+  @ApiOperation({
+    summary: 'List one table'
+  })
+  findOne(@Param('id')id: string): Promise<Table> {
+    return this.tableService.findOne(id)
   }
 
   @Post()
-  create(@Body() dto: CreateTableDto) {
+  @ApiOperation({
+    summary: 'Create a table'
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
   }
 }
